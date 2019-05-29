c3ba278064efe1d08e82fdf474e79d5f94ac0f1b
liangguifeng <1476982312@qq.com>
2019-05-29 19:08:57 +0800

    【新增】用户注册页面和路由

--- routes/web.php
+++ routes/web.php
@@ -15,3 +15,6 @@ Route::get('/help', 'StaticPagesController@help')->name('help');
 
 //关于
 Route::get('/about', 'StaticPagesController@about')->name('about');
+
+//登录(laravel兼容这种写法，和/signup并无区别)
+Route::get('signup', 'UsersController@create')->name('signup');
