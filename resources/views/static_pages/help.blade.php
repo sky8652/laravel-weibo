7b8b11a188609ac78c0f5469332ee23f00e5dee4
liangguifeng <1476982312@qq.com>
2019-05-29 16:37:31 +0800

    【新增】基本页面的创建，使用的模板继承

---
+++ resources/views/static_pages/help.blade.php
@@ -0,0 +1,10 @@
+<!-- 引入父视图-->
+@extends('layouts.default')
+
+<!-- 传递title的值为“帮助”-->
+@section('title', '帮助')
+
+<!-- 传递content的值为“<h1>帮助页</h1>”-->
+@section('content')
+    <h1>帮助页</h1>
+@stop
---
+++ resources/views/static_pages/home.blade.php
@@ -0,0 +1,10 @@
+<!-- 引入父视图-->
+@extends('layouts.default')
+
+<!-- 传递title的值为“主页”-->
+@section('title', '主页')
+
+<!-- 传递content的值为“<h1>主页</h1>”-->
+@section('content')
+    <h1>主页</h1>
+@stop
