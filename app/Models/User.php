2732d1b1ac1deb4f87d6e6c81234a0f6b839cc1f
liangguifeng <1476982312@qq.com>
2019-05-30 18:15:29 +0800

    【新增】在首页展示微博列表，以及发布微博实时更新在下方列表中

--- app/Models/User.php
+++ app/Models/User.php
@@ -63,4 +63,11 @@ class User extends Authenticatable
     {
         return $this->hasMany(Status::class);
     }
+
+    //获取当前用户关注的人发布过的所有微博动态,发布即更新
+    public function feed()
+    {
+        return $this->statuses()
+            ->orderBy('created_at', 'desc');
+    }
 }
