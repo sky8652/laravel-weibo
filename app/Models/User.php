f1c73a134a78ca40865fec06aedc3e908e1227c9
liangguifeng <1476982312@qq.com>
2019-05-30 18:42:15 +0800

    【新增】粉丝数据模型的多对多关系，以及创建数据表迁移文件

--- app/Models/User.php
+++ app/Models/User.php
@@ -71,4 +71,40 @@ class User extends Authenticatable
         return $this->statuses()
             ->orderBy('created_at', 'desc');
     }
+
+    //多对多关系
+    public function followers()
+    {
+        return $this->belongsToMany(User::Class, 'followers', 'user_id', 'follower_id');
+    }
+
+    //多对多关系
+    public function followings()
+    {
+        return $this->belongsToMany(User::Class, 'followers', 'follower_id', 'user_id');
+    }
+
+    //关注
+    public function follow($user_ids)
+    {
+        if ( ! is_array($user_ids)) {
+            $user_ids = compact('user_ids');
+        }
+        $this->followings()->sync($user_ids, false);
+    }
+
+    //取消关注
+    public function unfollow($user_ids)
+    {
+        if ( ! is_array($user_ids)) {
+            $user_ids = compact('user_ids');
+        }
+        $this->followings()->detach($user_ids);
+    }
+
+    //判断A用户是否关注了B用户
+    public function isFollowing($user_id)
+    {
+        return $this->followings->contains($user_id);
+    }
 }
